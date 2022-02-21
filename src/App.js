3fd53a25cfbf063a13dce307c36e6cc82c75bbd3
Víctor Bolaños Solís <viktorb.132@gmail.com>
2022-02-21 13:51:31 -0600

    Svg logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 import { GlobalStyle } from './GlobalStyle'
 
 export const App = () => (
     <>
         <GlobalStyle />
+        <Logo />
         <ListOfCategories />
         <ListOfPhotoCards />
     </>
