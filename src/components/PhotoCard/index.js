a0614f1096876f797649f055380bfc9fe6becaea
Víctor Bolaños Solís <viktorb.132@gmail.com>
2022-02-21 21:15:37 -0600

    Lazy load with hooks

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,21 +1,50 @@
-import React from 'react';
-import { Button, Img, ImgWrapper } from './styles';
+import React, { useEffect, useRef, useState } from 'react';
+import { Article, Button, Img, ImgWrapper } from './styles';
 import { MdFavoriteBorder } from 'react-icons/md'
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_150/v1555671700/category_cats.jpg'
 
 export const PhotoCard = ({ id, likes = 0, src=DEFAULT_IMAGE }) => {
-    return (
-        <article>
-            <a href={`/detail/${id}`} >
-                <ImgWrapper>
-                    <Img src={src} />
-                </ImgWrapper>
-            </a>
 
-            <Button>
-                <MdFavoriteBorder size='25px' />{ likes } likes!
-            </Button>
-        </article>
+    const element = useRef(null);
+    const [show, setShow] = useState(false)
+
+    useEffect(function () {
+        // console.log(element.current);
+        Promise.resolve(
+            typeof window.IntersectionObserver !== 'indefined'
+                ? window.IntersectionObserver
+                : import('intersection-observer')
+        ).then(() => {
+            const observer = new window.IntersectionObserver(function (entries) {
+                // console.log(entries);
+                const { isIntersecting } = entries[0]
+                // console.log(isIntersecting);
+                if (isIntersecting) {
+                    // console.log('si');
+                    setShow(true)
+                    observer.disconnect()
+                }
+            })
+            observer.observe(element.current)
+        })
+    }, [element])
+
+    return (
+        <Article ref={element}>
+            {
+                show && <>
+                    <a href={`/detail/${id}`} >
+                        <ImgWrapper>
+                            <Img src={src} />
+                        </ImgWrapper>
+                    </a>
+
+                    <Button>
+                        <MdFavoriteBorder size='25px' />{ likes } likes!
+                    </Button>
+                </>
+            }
+        </Article>
     )
 }
