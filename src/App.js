8211fd9b87e161036bfc5d7f25e82e8b5adbad1d
Víctor Bolaños Solís <viktorb.132@gmail.com>
2022-02-22 20:27:25 -0600

    Render props - get single image

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 import { Logo } from './components/Logo'
+import { PhotoCardWithQuery } from './containers/PhotoCardWithQuery'
 import { GlobalStyle } from './styles/GlobalStyle'
 
-export const App = () => (
-    <>
-        <GlobalStyle />
-        <Logo />
-        <ListOfCategories />
-        <ListOfPhotoCards categoryId={1} />
-    </>
-)
+export const App = () => {
+
+    const urlParams = new window.URLSearchParams(window.location.search)
+    const detailId = urlParams.get('detail')
+    console.log(detailId);
+    return (
+        <>
+            <GlobalStyle />
+            <Logo />
+            {
+                detailId
+                    ? <PhotoCardWithQuery id={detailId} />
+                    : <>
+                        <ListOfCategories />
+                        <ListOfPhotoCards categoryId={1} />
+                    </>
+            }
+        </>
+    )
+}
